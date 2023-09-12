3774b377665485b12e5c928eeb29449f281164c9
Chung Nguyen <zrmedia9@gmail.com>
2023-09-12 15:15:43 +0700

    Done section 3

---
+++ src/helpers/check.connect.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const { default: mongoose } = require("mongoose")
+const _SECONDS = 5000;
+
+const countConnect = () => {
+  const numConnection = mongoose.connections.length;
+  console.log(`Total connect: ${numConnection}`);
+}
+
+const checkOverload = () => {
+  setInterval(() => {
+    const numConnection = mongoose.connections.length;
+    const numCores = os.cpus().length;
+    const memoryUsage = process.memoryUsage().rss;
+    const maxConnections = numCores * 5;
+
+    console.log(`Active connection: ${numConnection}`);
+    console.log(`Memory usage: ${memoryUsage / 1024 / 1024} MB`);
+
+    if (numConnection > maxConnections) {
+      console.log(`Connection overload detected.`);
+    }
+  }, 5000);
+}
+
+module.exports = {
+  countConnect,
+  checkOverload,
+}
